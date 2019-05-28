9c73fba917b0a9e78b3649a0225e5c3c351d8672
Dirk Gustke <dirk.gustke@verticalmedia.com>
2019-05-28 13:36:01 +0200

    initial commit

---
+++ src/TaskInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Psr\EventDispatcher;
+
+/**
+ * This is a marker interface used to identify task events.
+ */
+interface TaskInterface extends EventInterface
+{
+}
